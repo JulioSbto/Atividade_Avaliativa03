cb1fbf11a5f0b6500876d436fcacf155d41991b1
Júlio César <juliosbto@gmail.com>
2025-06-13 07:30:23 -0300

    v1.2
    Front-end dos cards finalizados, estrutura do código modificada, correção de IDS repetidas e erros de digitação, funções adicionadas.

--- publico/funcoes.js
+++ publico/funcoes.js
@@ -1,7 +1,29 @@
-function carregarFuncionario(){
-    $("").append()
-}
+$(document).ready(function () {
 
-$(document).ready(function(){
+// Ocuta td
+  $("#tela_escura section").hide();
+  $("#tela_escura").hide();
 
-})
+// Esconde pag inicial qnd clicar em algo do menu
+  $(".btn_menu").click(function () {
+    const target = $(this).data("target");
+    $("#pagina_inicial").hide();
+
+// Puxa o target e mostra o card 
+    $("#tela_escura").css("display", "flex");
+    $("#" + target).css("display", "flex");
+  });
+
+//Oculta o card e puxa a pag inicial dnv
+  $(".btn_fechar").click(function () {
+    $("#tela_escura").hide();
+    $("#tela_escura section").hide();
+    $("#pagina_inicial").show();
+  });
+
+// Redirecionamento p logout
+  $("#btn_sair").click(function () {
+    window.location.href = "logout.html";
+  });
+
+});
